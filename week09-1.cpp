7eefc4fcca307ff66c020247496258e89a985907
broche079 <lary950709@gmail.com>
2025-04-18 11:56:40 +0800

    week09

---
+++ week09-1.cpp
@@ -0,0 +1,23 @@
+///Leet Code38. Count and Say
+///计硈尿瞷碞家ラRLEよ猭 絪絏 Θ 计+ダ Α
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return"1";
+        string prev = countAndSay(n-1); //ㄧΑ㊣ㄧΑ
+        string ans = "";
+        char prevC = prev[0]; //玡ダ
+        int prevN = 1; //玡ダ仓縩瞷碭Ω
+        for(int i=1; i<prev.length(); i++){
+            if(prevC == prev[i]) prevN++; //碞+1
+            else{ //ダぃ
+                ans += string(to_string(prevN)) + prevC; // 瞷碭Ω + êダ(癳ぇ玡仓縩ダ)
+                prevC = prev[i]; //穝ダ
+                prevN = 1; //眖1秨﹍(穝ダΤ)
+            }
+        }
+        ans += string(to_string(prevN)) + prevC;  //程掸璶癳
+        return ans;
+    }
+};
