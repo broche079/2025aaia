0d43c8a7e03c3f1e25ccf2c3cbf013c9487ab075
broche079 <lary950709@gmail.com>
2025-04-18 12:05:42 +0800

    week01

---
+++ week01/week01-1.cpp
@@ -0,0 +1,21 @@
+///week01-1.cpp 要自己打好全部的檔案
+ ///在瘋狂程設(練習模式)寫好的程式
+ ///codeblocks file-new-emp
+
+
+
+ #include <stdio.h>
+
+
+
+
+ int main()
+ {
+     int a, b;
+
+     scanf("%d%d", &a, %b);
+
+     printf("%d", a+b );
+
+
+ }
